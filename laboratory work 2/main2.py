43d79f74bc2a87fcc9269f1eb8212be42f064c2a
Roman <98983516+serguntsov@users.noreply.github.com>
2023-03-06 20:32:06 +0300

    Create main2.py

---
+++ laboratory work 2/main2.py
@@ -0,0 +1,10 @@
+import requests
+city = "Moscow,RU"
+appid = "cdf778b5f5669d8f94e12476be052125"
+res = requests.get("http://api.openweathermap.org/data/2.5/weather", params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Город:", city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
