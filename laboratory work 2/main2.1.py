7c19d9b9d9818a4071467073ee0731f6cb10734d
Roman <98983516+serguntsov@users.noreply.github.com>
2023-03-06 20:49:06 +0300

    Create main2.1.py

---
+++ laboratory work 2/main2.1.py
@@ -0,0 +1,9 @@
+import requests
+city = "Moscow,RU"
+appid = "cdf778b5f5669d8f94e12476be052125"
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast", params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <", i['weather'][0]['description'], ">")
+    print("____________________________")
