fdf04e7d269cd283453b28b5be3d503a4171e29b
Roman <98983516+serguntsov@users.noreply.github.com>
2023-03-30 00:22:42 +0300

    Update app.py
    Добавлена регистрация

--- laboratory work 3/app.py
+++ laboratory work 3/app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -12,19 +12,37 @@ conn = psycopg2.connect(database="service_db",
 
 cursor = conn.cursor()
 
-@app.route('/login/', methods=['GET'])
-def index():
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')   
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+            if records:
+                return render_template('account.html', full_name=records[0][1], login=records[0][2], password=records[0][3])
+            elif username == "" or password == "":
+                return "You did not enter your username or password! Try again or register."
+            else:
+                return "Incorrect login or password! Try again or register."
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+
     return render_template('login.html')
 
-@app.route('/login/', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-    records = list(cursor.fetchall())
-    if records:
-        return render_template('account.html', full_name=records)
-    elif username == "" or password == "":
-        return "You did not enter your username or password"
-    else:
-        return "Incorrect login or password"
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+        if len(name)==1 or len(login)==1 or len(password)==1:
+            return "You entered an empty value! Try again"
+        else:
+            cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);', (str(name), str(login), str(password)))
+            conn.commit()
+        return redirect('/login/')
+    return render_template('registration.html')
